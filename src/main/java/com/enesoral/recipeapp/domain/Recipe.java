da8c7c5332cde4ae3cb484bdd1bb15ff209ce541
enesoral <oralenes0@gmail.com>
2020-02-10 22:33:01 +0300

    add data on startup

--- src/main/java/com/enesoral/recipeapp/domain/Recipe.java
+++ src/main/java/com/enesoral/recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.enesoral.recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -15,6 +16,8 @@ public class Recipe {
     private Integer servings;
     private String source;
     private String url;
+
+    @Lob
     private String directions;
 
     @Lob
@@ -27,13 +30,13 @@ public class Recipe {
     private Note note;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredients;
+    private Set<Ingredient> ingredients = new HashSet<>();
 
     @ManyToMany
     @JoinTable(name = "recipe_category",
             joinColumns = @JoinColumn(name = "recipe_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<Category> categories;
+    private Set<Category> categories = new HashSet<>();
 
     public Long getId() {
         return id;
